2c6eaabc50b4b69069516c9cba345df4cac9c08b
Levi Broadnax <levibroadnax@gmail.com>
2019-05-16 21:12:29 -0700

    Updated cat component

--- client/src/components/Cat.jsx
+++ client/src/components/Cat.jsx
@@ -5,10 +5,13 @@ import React, { Component } from "react";
 class Cat extends Component {
   constructor(props) {
       super(props);
-      this.state = { 
-        ID: '',
+      this.state = {
+        maxCatID:1565964,
+        ID: 1,
         name: '',
-        svgURL: ''
+        svgURL: '',
+        ownerID: '',
+        ownerName: ''
       };
       this.inputChange = this.inputChange.bind(this);
       this.inputUpdate = this.inputUpdate.bind(this);
@@ -16,52 +19,75 @@ class Cat extends Component {
 
   inputChange (event) {
     event.preventDefault();
+    let catID = Number(event.target.value);
+    if(catID === this.state.ID || !Number.isInteger(catID)) return;
     this.setState({
-        ID: event.target.value
+        ID: catID
     });
   }
 
   inputUpdate = async (event) => {
     event.preventDefault();
-    let kitty = await this.GetCat(this.state.ID);
+    let catID = Number(event.target.value);
+    if(catID !== this.state.ID || !Number.isInteger(catID)) return;
+    event.preventDefault();
+    await this.GetCat(catID).then(res=>{
+      this.setState({
+        ID: res.id,
+        name: res.name,
+        svgURL: res.image_url_cdn,
+        ownerID: res.owner.address,
+        ownerName: res.owner.nickname,
+      });
+    });
   }
 
-  run = async () => {
-    const { } = this.state;
-
-    this.setState({  });
-  };
+  handleErrors(res) {
+    if(!res.ok)
+      throw Error(res.statusText);
+    return res;
+  }
 
   GetCat = async (catID) => {
     const route = `https://api.cryptokitties.co/kitties/${catID}`;
-    return fetch("https://api.cryptokitties.co/kitties/1401371", {
-      "credentials":"omit",
-      "headers":{ "accept":"application/json, text/plain, */*" },
-      "referrer":"https://www.cryptokitties.co/kitty/1401371",
-      "referrerPolicy":"no-referrer-when-downgrade",
+    return fetch(route, {
       "body":null,
+      "credentials":"omit",
+      "headers":{ "accept":"application/json" },
       "method":"GET",
-      "mode":"cors"
-    }).then(response => {
-      return response
+      "mode":"cors",
+      "referrer":route,
+      "referrerPolicy":"no-referrer-when-downgrade",
+    }).then(this.handleErrors)
+    .then(response => {
+      return response.json().then(data => {
+        return data;
+      });
+    }).catch(err => {
+      alert('cat does not exist');
     });
-  };
+  }; 
 
   render() {
-    if(!this.state.catID) {
-      return(
-        <div className={this.props.className}>
-          <input type="number" 
-                 name="ID"
-                 placeholder={this.props.className} 
-                 value={this.state.ID}
-                 onChange={this.inputChange}
-                 onBlur={this.inputUpdate} />
-        </div>
-      );
-    }
-    return (
-        <div className={this.props.className}>{this.props.className}</div>
+    return(
+      <div className={this.props.className}
+           style={{
+            'backgroundImage': 'url('+this.state.svgURL+'), none',
+            'backgroundRepeat': 'no-repeat',
+            // 'width':'100%',
+            // 'height':'100%',
+            'backgroundPosition':'center'
+          }}>
+        <input type="number" 
+               name="ID"
+               placeholder={this.props.className} 
+               value={this.state.ID}
+               onChange={this.inputChange}
+               onBlur={this.inputUpdate}
+               min="1"
+               max={this.state.maxCatID}
+               step={Math.floor(Math.random()*100000)} />
+      </div>
     );
   }
 }
